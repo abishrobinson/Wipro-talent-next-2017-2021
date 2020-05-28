2b52f9e871e1cfefcb48285f28c46f208af9781b
abishrobinson <65943124+abishrobinson@users.noreply.github.com>
2020-05-28 21:02:39 +0530

    Add files via upload

---
+++ prog.java
@@ -0,0 +1,19 @@
+1)
+public class Main
+{
+	public static void main(String args[]) {
+		System.out.println(args[0] +" Technologies " +args[1]);
+	}
+}
+
+
+
+
+
+2)
+public class Main
+{
+	public static void main(String args[]) {
+		System.out.println(" Welcome " +args[0]);
+	}
+}
